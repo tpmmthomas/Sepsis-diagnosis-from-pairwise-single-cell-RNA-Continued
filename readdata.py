d32b4c814beef3c60c92d6d798624c7896ed6f79
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2020-10-01 12:39:59 +0800

    Update readdata.py

--- readdata.py
+++ readdata.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 
 print("start")
-z = np.empty((0,122558)) # Will have memory error
-for df in pd.read_csv(r"data/scp_gex_matrix.csv.gz",compression ="gzip", chunksize=1):
+z = np.empty((0,122558)) # Will have memory error soon
+for df in pd.read_csv(r"data/scp_gex_matrix.csv.gz",compression ="gzip", chunksize=50):
     df = df[(df.T != 0).any()] # remove all zero rows
     print(df)
     arr = df.to_numpy()
