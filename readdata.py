d30aaa3016f8790a3880b82d37a02c3d39d0a67f
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2020-10-01 12:18:07 +0800

    Update readdata.py

--- readdata.py
+++ readdata.py
@@ -4,10 +4,13 @@ import numpy as np
 print("start")
 z = np.empty((0,122558))
 for df in pd.read_csv(r"data/scp_gex_matrix.csv.gz",compression ="gzip", chunksize=50):
-    df = df[(df.T != 0).any()]
+    df = df[(df.T != 0).any()] # remove all zero rows
     print(df)
     arr = df.to_numpy()
     z = np.vstack([z,arr])
     print(z)
 
+idx = np.argwhere(np.all(z[..., :] ==0,axis = 0)) #find index of zero columns
+z = np.delete(z,idx,axis = 1) #remove all zero columns
 
+#continue processing on z
