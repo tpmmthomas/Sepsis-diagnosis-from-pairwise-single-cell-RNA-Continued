ec066d4fe39bf5825fc59562f21fc3e7f26831d9
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2020-10-01 13:23:21 +0800

    Update readdata.py

--- readdata.py
+++ readdata.py
@@ -3,11 +3,11 @@ import numpy as np
 
 print("start")
 z = np.empty((0,122558)) # Will have memory error soon
-for df in pd.read_csv(r"data/scp_gex_matrix.csv.gz",compression ="gzip", chunksize=50):
-    df = df[(df.T != 0).any()] # remove all zero rows
+for df in pd.read_csv(r"data/scp_gex_matrix.csv.gz",compression ="gzip", chunksize=10):
+    df = df[(df.loc[:, df.columns != "GENE"].T != 0).any()] # remove all zero rows
     print(df)
     arr = df.to_numpy()
-    z = np.vstack([z,arr])
+    z = np.vstack([z,arr]) # error after 450 rows 
     print(z)
 
 idx = np.argwhere(np.all(z[..., :] ==0,axis = 0)) #find index of zero columns
