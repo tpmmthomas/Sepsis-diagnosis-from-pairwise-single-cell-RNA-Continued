6dd99d91f570d145e99cb834a1318f0446fdd0ab
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2020-10-01 12:17:15 +0800

    Update readdata.py

--- readdata.py
+++ readdata.py
@@ -3,10 +3,10 @@ import numpy as np
 
 print("start")
 z = np.empty((0,122558))
-for df in pd.read_csv(r"data/scp_gex_matrix.csv.gz",compression ="gzip", chunksize=100 ):
+for df in pd.read_csv(r"data/scp_gex_matrix.csv.gz",compression ="gzip", chunksize=50):
     df = df[(df.T != 0).any()]
     print(df)
-    arr = df.tonumpy()
+    arr = df.to_numpy()
     z = np.vstack([z,arr])
     print(z)
 
