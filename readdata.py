92286c7ba2111e265f94036ddf9a8fbd684ac13c
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2020-10-01 10:10:52 +0800

    Update readdata.py

--- readdata.py
+++ readdata.py
@@ -1,6 +1,9 @@
+import pandas as pd
 
+i=0
+print("start")
+for df in pd.read_csv(r"data/scp_gex_matrix.csv.gz",compression ="gzip", chunksize=100 ):
+    print(i)
+    i = i + 1
+    print(df)
 
-x = 5
-print(x)
-
-#USE NUMPY
