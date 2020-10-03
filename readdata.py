707ad76e3e51e2e6e9081d816f072b48f3f9fcba
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2020-10-03 17:58:29 +0800

    Update readdata.py

--- readdata.py
+++ readdata.py
@@ -7,18 +7,20 @@ filename = r"data/scp_gex_matrix.csv.gz"
 print("start")
 with gzip.open(filename, mode="rt") as f:
     reader = csv.reader(f)
-    i = next(reader)
-print(i)
-z = np.empty((0,122558)) 
+    i = next(reader) #stores name of each sample
 
-for df in pd.read_csv(filename,compression ="gzip", chunksize=10):
+col_name = np.array(i)
+print(col_name)
+data = np.empty((0,122558)) 
+
+for df in pd.read_csv(filename,compression ="gzip", chunksize=1):
     df = df[(df.loc[:, df.columns != "GENE"].T != 0).any()] # remove all zero rows
     print(df)
     arr = df.to_numpy()
-    z = np.vstack([z,arr]) # error after 450 rows 
-    print(z)
+    data = np.vstack([data,arr]) 
+    print(data)
 
-idx = np.argwhere(np.all(z[..., :] ==0,axis = 0)) #find index of zero columns
+idx = np.argwhere(np.all(data[..., :] ==0,axis = 0)) #find index of zero columns
 z = np.delete(z,idx,axis = 1) #remove all zero columns
 
 #continue processing on z
