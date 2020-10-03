2740ff6097fad48a8a6dfdba40ec9b937ea0959e
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2020-10-03 20:43:56 +0800

    Update readdata.py

--- readdata.py
+++ readdata.py
@@ -26,7 +26,7 @@ label = df.to_numpy()
 print("Samples ",samples.shape)
 print("Labels ",label.shape)
 
-#delete all zero columns
+#delete all zero columns (not yet done before right?)
 idx = np.argwhere(np.all(samples[1:] ==0,axis = 0)) #find index of zero columns
 samples = np.delete(samples,idx,axis = 1) 
 
@@ -38,23 +38,33 @@ samples = samples[1:]
 
 #split training and testing sample (x = sample, y = label )
 x_train,x_test,y_train,y_test = train_test_split(samples,label,test_size = 0.1, random_state = 41)
+print("Split succcessful")
 
 #separate control and case samples
 con_sample = np.empty((0,len(sample[0])))
 case_sample = np.empty((0,len(sample[0])))
+i = 0
 for lb in label:
     if lb == "Control":
         con_sample = np.vstack([con_sample,sample[i]])
     else:
         case_sample = np.vstack([con_sample,sample[i]])
     i = i + 1
-    
+
+print("Separation succcessful")
+
 # optain stat value
 t_stat,pvalue = stats.ttest_ind(con_sample, case_sample, axis = 0, equal_var=True, nan_policy='raise')
 rejected, P_fdr = fdrcorrection(pvalue, alpha=0.05, method='indep', is_sorted=False)
 
+# select RNAs with small P_fdr as the input
+i = 0
+for fdr in P_fdr:
+    if fdr > 0.02:
+        x_train = np.delete(x_train,i,1)
+        x_test = np.delete(x_test,i,1)
+        rna_names = np.delete(rna_names,i,1)
 
-
-
+print("Filter succcessful")
 
 
