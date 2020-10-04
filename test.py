144a9cb6bd0ca142a59953855a705713a4188452
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2020-10-04 21:21:05 +0800

    gh

--- test.py
+++ test.py
@@ -8,19 +8,22 @@ from sklearn.model_selection import train_test_split
 import gzip
 import csv 
 
+def row_count(input):
+    i = 0
+    for df in  pd.read_csv(input,compression ="gzip", chunksize = 1, header = None):
+        arr = df.to_numpy()
+        print(arr)
+        i = i + 1
+        if i >= 2:
+            break
 
 xt_loc = r"data/training_sample_raw.csv.gz"
 xtt_loc = r"data/testing_sample_raw.csv.gz"
 yt_loc = r"data/training_label_raw.csv.gz"
 ytt_loc = r"data/testing_label_raw.csv.gz"
 
-print(row_count(ytt_loc))
-print(row_count(yt_loc))
-print(row_count(xtt_loc))
-print(row_count(xt_loc))
+row_count(ytt_loc)
+row_count(yt_loc)
+row_count(xtt_loc)
+row_count(xt_loc)
 
-def row_count(input):
-    with gzip.open(input,"rt") as f:
-        for i, l in enumerate(f):
-            pass
-    return i
